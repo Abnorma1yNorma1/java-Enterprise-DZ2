8bd70e2ab177784f712dc044b97d337c3efb225f
Evgeny Prihodzko <evgenyprihodzko@gmail.com>
2025-05-29 22:57:39 +0300

    написал вывод значений из ДЗ

--- src/main/java/by/it_academy/jd2/Mk_jd2_111_25/FinServlet.java
+++ src/main/java/by/it_academy/jd2/Mk_jd2_111_25/FinServlet.java
@@ -17,6 +17,7 @@ public class FinServlet extends HttpServlet {
         String[] rows = req.getParameterValues("rows");
 
         PrintWriter writer = resp.getWriter();
+
         writer.write("<table border='1'>");
         for (String row : rows) {
             String[] cells = row.split(",");
@@ -29,5 +30,34 @@ public class FinServlet extends HttpServlet {
             writer.write("</tr>");
         }
         writer.write("</table>");
+
+        int totalNumber = 0;
+        long totalSum = 0;
+        for (String row: rows){
+            String[] cells = row.split(",");
+            totalSum = totalSum + Long.parseLong(cells[0])*Long.parseLong(cells[1]);
+            totalNumber = totalNumber + Integer.parseInt(cells[1]);
+        }
+        long averagePrice = totalSum/totalNumber;
+        writer.write("<p>");
+        writer.write("<div id=average-price-container>\n" +
+                            "<div class=\"medium-title\">\n" +
+                            "Средняя цена\n" +
+                            "</div>\"" +
+                            "<div class=\"fs-2 green\">" + averagePrice + "</div>" +
+                        "</div>");
+        writer.write("<div id=total-count-container>\n" +
+                            "<div class=\"medium-title\">\n" +
+                            "Общая численность\n" +
+                            "</div>\n" +
+                            "<div class=\"fs-2\">" + totalNumber + "</div>" +
+                        "</div>");
+        writer.write("<div id=total-amount-container>\n" +
+                            "<div class=\"medium-title\">\n" +
+                            "Общая сумма\n" +
+                            "</div>\n" +
+                            "<div class=\"fs-2\">" + totalSum + "</div>" +
+                        "</div>");
+        writer.write("</p>");
     }
 }
